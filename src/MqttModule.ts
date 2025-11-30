bde06bfcd9719e01a04488b282d401ab315c02b4
coderscode2 <veda59@gmail.com>
2025-11-30 15:32:33 -0700

    Create MqttModule.ts

---
+++ src/MqttModule.ts
@@ -0,0 +1,46 @@
+import { NativeModules } from 'react-native';
+import type { MqttCertificates } from './types';
+
+interface MqttModuleType {
+  connect(
+    broker: string,
+    clientId: string,
+    certificates: MqttCertificates,
+    successCallback: (message: string) => void,
+    errorCallback: (error: string) => void
+  ): void;
+  disconnect(
+    successCallback: (message: string) => void,
+    errorCallback: (error: string) => void
+  ): void;
+  subscribe(
+    topic: string,
+    qos: number,
+    successCallback: (message: string) => void,
+    errorCallback: (error: string) => void
+  ): void;
+  unsubscribe(
+    topic: string,
+    successCallback: (message: string) => void,
+    errorCallback: (error: string) => void
+  ): void;
+  publish(
+    topic: string,
+    message: string,
+    qos: number,
+    retained: boolean,
+    successCallback: (message: string) => void,
+    errorCallback: (error: string) => void
+  ): void;
+  isConnected(callback: (isConnected: boolean) => void): void;
+}
+
+const { MqttModule } = NativeModules;
+
+if (!MqttModule) {
+  throw new Error(
+    'MqttModule native module not found. Make sure you have properly linked the native module and rebuilt your app.'
+  );
+}
+
+export default MqttModule as MqttModuleType;
