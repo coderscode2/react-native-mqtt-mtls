dcb3056d6d8ec518104ff207bbc5cbc76c4995eb
coderscode2 <veda59@gmail.com>
2025-12-13 18:59:51 -0700

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -9,7 +9,7 @@ declare module 'react-native-mqtt-mtls' {
 
   export interface MqttCertificates {
     clientCert: string;
-    privateKeyAlias: string;
+    privateKey: string;      // ← CHANGED from privateKeyAlias
     rootCa: string;
   }
 
@@ -49,21 +49,25 @@ declare module 'react-native-mqtt-mtls' {
       successCallback: (message: string) => void,
       errorCallback: (error: string) => void
     ): void;
+
     disconnect(
       successCallback: (message: string) => void,
       errorCallback: (error: string) => void
     ): void;
+
     subscribe(
       topic: string,
       qos: number,
       successCallback: (message: string) => void,
       errorCallback: (error: string) => void
     ): void;
+
     unsubscribe(
       topic: string,
       successCallback: (message: string) => void,
       errorCallback: (error: string) => void
     ): void;
+
     publish(
       topic: string,
       message: string,
@@ -72,6 +76,7 @@ declare module 'react-native-mqtt-mtls' {
       successCallback: (message: string) => void,
       errorCallback: (error: string) => void
     ): void;
+
     isConnected(callback: (isConnected: boolean) => void): void;
   }
 
